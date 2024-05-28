73fdbf759a34f8753ef91f987e1778e3bc69a267
Ewa Roszczyk <ewakae.au@gmail.com>
2024-05-28 22:43:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ import numpy as np
 @st.cache_data(persist=True)
 def load_data_in_chunks(file_path, chunk_size=100000):
     chunk_list = []
+    required_columns = ['ARRIVAL_TIME', 'DATE', 'ARRIVAL_DELAY', 'DEPARTURE_DELAY']
     for chunk in pd.read_csv(file_path, chunksize=chunk_size, low_memory=False):
-        chunk['ARRIVAL_TIME'] = pd.to_datetime(chunk['ARRIVAL_TIME'], format='%H:%M:%S')
-        chunk['DATE'] = pd.to_datetime(chunk['DATE'])
-        chunk['ARRIVAL_DELAY'] = chunk['ARRIVAL_DELAY'].astype(np.float32)
-        chunk['DEPARTURE_DELAY'] = chunk['DEPARTURE_DELAY'].astype(np.float32)
-        chunk_list.append(chunk)
-    df = pd.concat(chunk_list, ignore_index=True)
+        if all(column in chunk.columns for column in required_columns):
+            chunk['ARRIVAL_TIME'] = pd.to_datetime(chunk['ARRIVAL_TIME'], format='%H:%M:%S', errors='coerce')
+            chunk['DATE'] = pd.to_datetime(chunk['DATE'], errors='coerce')
+            chunk['ARRIVAL_DELAY'] = chunk['ARRIVAL_DELAY'].astype(np.float32, errors='ignore')
+            chunk['DEPARTURE_DELAY'] = chunk['DEPARTURE_DELAY'].astype(np.float32, errors='ignore')
+            chunk_list.append(chunk)
+        else:
+            st.warning("Some chunks are missing required columns and will be skipped.")
+    if chunk_list:
+        df = pd.concat(chunk_list, ignore_index=True)
+    else:
+        st.error("No valid data loaded. Please check your dataset.")
+        df = pd.DataFrame(columns=required_columns)
     return df
 
 # Load dataset and store it in session state if not already present
 if 'df' not in st.session_state:
-    # st.session_state.df = load_data_in_chunks('2015_dataset/merged_1k_sample.csv')
     st.session_state.df = load_data_in_chunks('2015_dataset/merged_full_dataset.csv')
 
 show_pages(
